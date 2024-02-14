8a14199753b8f9122d2824ac010f14506f17a12c
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-14 10:38:43 +0100

    Add project files.

---
+++ inlämninguppgift6.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett tal");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv in ett tal");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(Störst(a,b)+" är det största talet");
+    }
+    static int Störst(int x, int y)
+    {
+        if(x>=y)
+        {
+            return x;
+        }
+        else
+        {
+            return y;
+        }
+    }
+}
